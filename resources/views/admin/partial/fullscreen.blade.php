714ed88e471073005cf23dcf4c5f277de2be7839
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2025-10-14 14:55:07 +0500

    Fix : 'Changes

--- resources/views/admin/partial/fullscreen.blade.php
+++ resources/views/admin/partial/fullscreen.blade.php
@@ -5,29 +5,3 @@
         <span class="d-none ms-2">Toggle Fullscreen</span>
     </a>
 </li>
-
-<script>
-    const fullscreenBtn = document.getElementById('btn-fullscreen');
-    const fullscreenIcon = document.getElementById('fullscreen-icon');
-
-    fullscreenBtn.addEventListener('click', async () => {
-        if (!document.fullscreenElement) {
-            try {
-                await document.documentElement.requestFullscreen();
-                fullscreenIcon.classList.replace('tabler-arrows-maximize', 'tabler-arrows-minimize');
-            } catch (err) {
-                console.error('Error attempting to enter fullscreen:', err);
-            }
-        } else {
-            await document.exitFullscreen();
-            fullscreenIcon.classList.replace('tabler-arrows-minimize', 'tabler-arrows-maximize');
-        }
-    });
-
-    // Optional: update icon if user exits fullscreen via ESC
-    document.addEventListener('fullscreenchange', () => {
-        if (!document.fullscreenElement) {
-            fullscreenIcon.classList.replace('tabler-arrows-minimize', 'tabler-arrows-maximize');
-        }
-    });
-</script>

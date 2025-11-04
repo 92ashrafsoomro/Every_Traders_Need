6b7c5265158a92b770bcee966403c8ee6783c35c
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2025-11-04 05:59:20 -0800

    New Components Vuetify

---
+++ resources/js/user/component/ComoboxInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-combobox
+        clearable
+        :label="label"
+        :items="items"
+        variant="outlined"
+    ></v-combobox>
+</template>
+
+<script>
+export default {
+    name: "ComoboxSelect",
+    props: {
+        label: {
+            type: String,
+            default: "Select Options",
+        },
+        items: {
+            type: Array,
+            default: () => [
+                "California",
+                "Colorado",
+                "Florida",
+                "Georgia",
+                "Texas",
+                "Wyoming",
+            ],
+        },
+    },
+};
+</script>
+
+<style></style>
---
+++ resources/js/user/component/newSelect.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-select
+        clearable
+        chips
+        :label="label"
+        :items="items"
+        multiple
+        variant="outlined"
+    />
+</template>
+<script>
+export default {
+    name: "CustomSelect",
+    props: {
+        label: {
+            type: String,
+            default: "Select Option",
+        },
+        items: {
+            type: Array,
+            default: () => [
+                "California",
+                "Colorado",
+                "Florida",
+                "Georgia",
+                "Texas",
+                "Wyoming",
+            ],
+        },
+    },
+};
+</script>
+
+<style></style>

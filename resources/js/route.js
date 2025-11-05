719cfae404fdbd271c2920d55bbecd272508a41f
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2025-11-05 07:33:00 -0800

    fix: "Route Updates with Login and BackToHome Btn"

--- resources/js/route.js
+++ resources/js/route.js
@@ -4,18 +4,22 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { useUserStore } from '@stores/userStore';
 import { useAlertStore } from '@stores/alertStore';
 
-
+import HomeView from "./web/pages/index.vue"
 import Login from "./user/pages/auth/login.vue"
 import NotFound from "./user/pages/404.vue"
 import UserRoutes from "./user/route.js"
 
 
 const routes = [
-    ...UserRoutes,
+    {
+        path: "/",
+        component: HomeView,
+    },
     {
         path: "/login",
         component: Login,
     },
+    ...UserRoutes,
     { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
 ];
 
@@ -27,15 +31,15 @@ const router = createRouter({
 
 
 router.beforeEach(async (to, from, next) => {
- 
+
     const auth = useUserStore()
     const alertStore = useAlertStore()
 
     //Auth Restriction
     if (to.meta.requiresAuth) {
-           
+
         try {
-                 
+
             const res = await auth.getProfile();
             auth.user = res.account;
             auth.is_logged_in = true;

bc0781a0bc97234cb1f07e6670236c3dfd33f203
MohsinSheeraz <syedmohsinsheeraz123@gmail.com>
2025-11-05 20:59:18 +0500

    update_code

--- resources/js/user/component/Layout/ThemeDropdown.vue
+++ resources/js/user/component/Layout/ThemeDropdown.vue
@@ -1,9 +1,7 @@
 <template>
     <v-menu class="menuLabel" location="bottom" transition="fade-transition">
         <template #activator="{ props }">
-            <v-btn icon v-bind="props">
-                <svg-icon type="mdi" :path="mdiCeilingLight"></svg-icon>
-            </v-btn>
+            <v-btn :icon="this.vuetify.global.name == 'adminDark' ? 'md:dark_mode' :'md:light_mode'" v-bind="props"></v-btn>
         </template>
 
         <v-list style="
@@ -11,47 +9,37 @@
                 color: #b2c0ce;
                 margin-left: -100px;
             ">
-            <v-list-item link>
-                <v-list-item-title class="cursor-pointer">
-                    <div class="d-flex align-center ga-2 text-white">
-                        <svg-icon type="mdi" :path="mdiGlobeLightOutline"></svg-icon>
-                        <div>Light Theme</div>
-                    </div>
-                </v-list-item-title>
+            <v-list-item color="primary" :variant="this.vuetify.global.name == 'adminLight' ? 'tonal' :'plain'" prepend-icon="md:light_mode" @click="toggleTheme()" link>
+                <v-list-item-title class="cursor-pointer">Light Theme </v-list-item-title>
             </v-list-item>
-            <v-list-item link>
-                <v-list-item-title class="cursor-pointer">
-                    <div class="d-flex align-center ga-2 text-white">
-                        <svg-icon type="mdi" :path="mdiGlobeLight"></svg-icon>
-                        <div>Dark Theme</div>
-                    </div>
-                </v-list-item-title>
+            <v-list-item color="primary" :variant="this.vuetify.global.name == 'adminDark' ? 'tonal':'plain'" prepend-icon="md:dark_mode" @click="toggleTheme()" link>
+                <v-list-item-title class="cursor-pointer">Dark Theme </v-list-item-title>
             </v-list-item>
         </v-list>
     </v-menu>
 </template>
 <script>
-import SvgIcon from "@jamescoyle/vue-icon";
-import {
-    mdiCeilingLight,
-    mdiGlobeLightOutline,
-    mdiGlobeLight,
-    mdiLaptop,
-} from "@mdi/js";
+import { useTheme } from "vuetify";
 
 export default {
     name: "IconMenu",
     components: {
-        SvgIcon,
+        
     },
     data() {
         return {
-            mdiCeilingLight,
-            mdiGlobeLightOutline,
-            mdiGlobeLight,
-            mdiLaptop,
+            vuetify: useTheme(),
         };
     },
+    methods: {
+        toggleTheme() {
+            if (this.vuetify.global.name == "adminDark") {
+                this.vuetify.change("adminLight");
+            } else {
+                this.vuetify.change("adminDark");
+            }
+        },
+    }
 };
 </script>
 <style></style>

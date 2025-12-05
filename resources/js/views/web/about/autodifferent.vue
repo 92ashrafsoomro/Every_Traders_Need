e2fdede2552e3c3b77631af8e51e4196edbdd67d
daprimeg <owais.dapp@gmail.com>
2025-12-05 15:45:08 +0500

    update

--- resources/js/views/web/about/autodifferent.vue
+++ resources/js/views/web/about/autodifferent.vue
@@ -1,60 +1,44 @@
 <template>
-    <div class="bg-surface pa-16">
-  <v-container style="max-width: 1500px; margin: auto;" >
-        <div class="text-center mb-12">
+  <div class="bg-surface pa-16">
+    <v-container style="max-width: 1500px; margin: auto;">
+      <div class="text-center mb-12">
         <h2 class="text-h3 text-md-h2 font-weight-bold text-white mb-4">
-        What Makes Autoboli Different
+          What Makes Autoboli Different
         </h2>
         <p class="text-subtitle-1 text-grey-lighten-1 max-w-800 mx-auto">
-         Unlike traditional valuation tools or static vehicle lookup platforms, Autoboli gives you
+          Unlike traditional valuation tools or static vehicle lookup platforms, Autoboli gives you
         </p>
       </div>
-    <v-row  >
+      <v-row>
 
-      <v-col
-        v-for="(msg, index) in messages"
-        :key="index"
-        cols="12"
-        sm="6"
-        md="4"
-        
-      >
-        <v-card class="h-100 rounded-xl">
+        <v-col v-for="(msg, index) in messages" :key="index" cols="12" sm="6" md="4">
+          <v-card class="h-100 rounded-xl">
 
-          <v-img
-            height="200"
-            src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
-            cover
-          >
-            
-          </v-img>
+            <v-img height="200" src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg" cover>
 
-          <v-card-text style="background-color: rgb(275, 275, 275 , 0.1);">
-          
+            </v-img>
 
-            <v-timeline align="start" density="compact" >
-              <v-timeline-item>
-                <strong>{{ msg.title }}</strong>
-                <div>{{ msg.desc }}</div>
+            <v-card-text style="background-color: rgb(275, 275, 275 , 0.1);">
 
-                <v-btn
-                  v-if="msg.hasButton"
-                  size="small"
-                  color="primary"
-                  variant="flat"
-                >
-                  {{ msg.buttonText }}
-                </v-btn>
 
-              </v-timeline-item>
-            </v-timeline>
-          </v-card-text>
+              <v-timeline align="start" density="compact">
+                <v-timeline-item>
+                  <strong>{{ msg.title }}</strong>
+                  <div>{{ msg.desc }}</div>
 
-        </v-card>
-      </v-col>
+                  <v-btn v-if="msg.hasButton" size="small" color="primary" variant="flat">
+                    {{ msg.buttonText }}
+                  </v-btn>
 
-    </v-row>
-  </v-container>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card-text>
+
+          </v-card>
+        </v-col>
+
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -92,7 +76,4 @@ const messages = [
 
 <style scoped>
 /* Perfect 2px gap between cards */
-.gap-2px {
-  gap: 2px !important;
-}
 </style>

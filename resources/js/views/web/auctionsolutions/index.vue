4814030658aad4259242dc6bd260a9ab892912eb
Raziq <raziqkhan4824@gmail.com>
2025-12-22 21:31:05 +0500

    raziq

--- resources/js/views/web/auctionsolutions/index.vue
+++ resources/js/views/web/auctionsolutions/index.vue
@@ -1,2 +1,132 @@
-<template></template>
+<template>
+    <div class="w-100 bg-primary " style="padding:100px 0 100px 0;">
+            <div class="mx-auto px-5"style="max-width: 1100px;">
+                <p class="text-h6 mb-4">Auction Solution</p>
+                <h1 class="text-h4 text-md-h2 font-weight-black mb-4">The Real Problems Dealers & Traders Face at Auctions And How Autoboli Solves Them</h1>
+                <h2 class="text-h5 text-md-h4  mb-4">Vehicle auctions are fast, competitive, and unpredictable. For many dealers and traders, one wrong bid can erase weeks of profit.</h2>
+                <p class="text-h6 text-md-h5">Autoboli exists to remove uncertainty from auction buying and replace it with clarity, confidence, and control.</p>
+            </div>
+    </div>
+
+    <div>
+        <div class="mx-auto"style="max-width: 1400px;">
+
+
+                    <div class="d-flex justify-center flex-wrap my-16 ga-16"  >
+                            <div  style=" width:500px;">
+                                    <h2 class="text-lg-h4 text-md-h4 mb-3 ">The Problems Dealers & Traders Face</h2>
+                                    <h2 class="text-lg-h5 text-md-h4 mb-3 ">1. Bidding Without Knowing the True Market Value</h2>
+                                    <p class="mb-3">Most traders bid based on guesswork, outdated prices, or limited data. </p>
+                                    <p class="mb-3">The risk:</p>
+                                    <ul class="mb-3 ml-5">
+                                        <li>Overpaying for stock</li>
+                                        <li>Low or zero profit margins</li>
+                                        <li>Losing money after fees and repairs</li>
+                                    </ul>
+                                    
+                                   
+                            </div>
+                            <div ><img src="c:\Users\USEER\Desktop\square-crop.png" width="100%" alt=""></div>
+                    </div>
+                     <div class="d-flex justify-center flex-wrap-reverse my-16 ga-16"  >
+                         <div ><img src="c:\Users\USEER\Desktop\square-crop.png" width="100%" alt=""></div>
+                             <div  style=" width:500px;">
+                                    <h2 class="text-lg-h4 text-md-h4 mb-3 ">The Problems Dealers & Traders Face</h2>
+                                    <h2 class="text-lg-h5 text-md-h4 mb-3 ">2. Data Is Scattered Across Too Many Sources</h2>
+                                    <p class="mb-3">Auction prices, valuations, condition reports, and history all live in different places.</p>
+                                    <p class="mb-3">The risk:</p>
+                                    <ul class="mb-3 ml-5">
+                                        <li>Hours wasted switching platforms</li>
+                                        <li>Missed opportunities</li>
+                                        <li>Incomplete decision-making</li>
+                                    </ul>
+
+                                 
+                            </div>
+                    </div>
+                    
+                    <div class="d-flex justify-center flex-wrap my-16 ga-16"  >
+                            <div  style=" width:500px;">
+                                    <h2 class="text-lg-h4 text-md-h4 mb-3 ">The Problems Dealers & Traders Face</h2>
+                                    <h2 class="text-lg-h5 text-md-h4 mb-3 ">3. Hidden Risks in Vehicle Condition</h2>
+                                    <p class="mb-3">Many vehicles look profitable at first glance — until unseen condition issues appear. </p>
+                                    <p class="mb-3">The risk:</p>
+                                    <ul class="mb-3 ml-5">
+                                        <li>Unexpected repair costs</li>
+                                        <li>Stock that doesn’t retail well</li>
+                                        <li>Vehicles stuck on the forecourt</li>
+                                    </ul>
+
+                                 
+                            </div>
+                            <div><img src="c:\Users\USEER\Desktop\square-crop.png" width="100%" alt=""></div>
+                    </div>
+                     <div class="d-flex justify-center flex-wrap-reverse my-16 ga-16"  >
+                         <div><img src="c:\Users\USEER\Desktop\square-crop.png" width="100%" alt=""></div>
+                            <div  style=" width:500px;">
+                                    <h2 class="text-lg-h4 text-md-h4 mb-3 ">The Problems Dealers & Traders Face</h2>
+                                    <h2 class="text-lg-h5 text-md-h4 mb-3 ">4. Reauction Vehicles Go Unnoticed</h2>
+                                    <p class="mb-3">Vehicles that fail to sell often reappear — sometimes with better value. </p>
+                                    <p class="mb-3">The risk:</p>
+                                    <ul class="mb-3 ml-5">
+                                        <li>Missing discounted reauction opportunities</li>
+                                        <li>Repeating others’ mistakes unknowingly</li>
+                                        
+                                    </ul>
+
+                                 
+                            </div>
+                    </div>
+                    
+                    <div class="d-flex justify-center flex-wrap my-16 ga-16"  >
+                            <div  style=" width:500px;">
+                                    <h2 class="text-lg-h4 text-md-h4 mb-3 ">The Problems Dealers & Traders Face</h2>
+                                    <h2 class="text-lg-h5 text-md-h4 mb-3 ">5. Choosing the Wrong Auction House</h2>
+                                    <p class="mb-3">Prices for the same vehicle vary widely between auction houses and regions. </p>
+                                    <p class="mb-3">The risk:</p>
+                                    <ul class="mb-3 ml-5">
+                                        <li>Buying in the wrong place</li>
+                                        <li>Lower resale profit</li>
+                                        <li>Poor buying strategy</li>
+                                    </ul>
+
+                                  
+                            </div>
+                            <div><img src="c:\Users\USEER\Desktop\square-crop.png" width="100%" alt=""></div>
+                    </div>
+                     <div class="d-flex justify-center flex-wrap-reverse my-16 ga-16"  >
+                         <div><img src="c:\Users\USEER\Desktop\square-crop.png" width="100%" alt=""></div>
+                            <div  style=" width:500px;">
+                                    <h2 class="text-lg-h4 text-md-h4 mb-3 ">The Problems Dealers & Traders Face</h2>
+                                    <h2 class="text-lg-h5 text-md-h4 mb-3 ">6. No Time to Plan Ahead</h2>
+                                    <p class="mb-3">Without clear auction schedules and alerts, many traders bid reactively.</p>
+                                    <p class="mb-3">The risk:</p>
+                                    <ul class="mb-3 ml-5">
+                                        <li>Missed lots</li>
+                                        <li>Poor planning</li>
+                                        <li>Emotional bidding</li>
+                                    </ul>
+
+                                 
+                            </div>
+                    </div>
+                    
+                    
+
+
+        
+    
+        </div>             
+    </div>
+</template>
 <script></script>
+<style>
+   
+    @media (max-width: 442px) {
+        
+       .image{
+        width:250px;
+       }
+      
+    }
+</style>

edf6635419049e1b63e113b7cfcb63b3b68cd7bb
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2026-01-08 14:32:38 +0500

    huzaifa

--- resources/js/views/admin/auction/csv/index.vue
+++ resources/js/views/admin/auction/csv/index.vue
@@ -1,78 +1,110 @@
 <template>
-        <v-card :loading="loading" :disabled="loading" class="my-3 border">
-                <div class="d-flex justify-space-between border-b py-3 px-4" >
-                    <div class="align-self-center" >
-                        <h1 class=" text-h6 Sheet">CSV</h1>
-                    </div>
-                    <div class="mx-3 d-flex">
-                        <div class="px-2"  >
-                            <v-icon @click="submit" style="background-color: rgb(var(--v-theme-primary),0.3); padding: 20px;" class="border" color="primary"  >mdi-check-decagram</v-icon>
-                        </div>
-                        <div class="px-2" >
-                            <v-icon style="background-color: rgb(var(--v-theme-background)); padding: 20px;" class="border" color="primary"  @click="handleFile()">mdi-database-check-outline</v-icon>
-                        </div>
-                        <div class="px-2" >
-                            <v-icon style="background-color: rgb(var(--v-theme-background)); padding: 20px;" class="border" color="primary"  @click="loadVehicle()">mdi-restart</v-icon>
-                        </div>
-                    </div>
+    <v-card :loading="loading" :disabled="loading" class="my-3 border ">
+        <div class="d-flex justify-space-between border-b py-3 px-4">
+            <div class="align-self-center">
+                <h1 class=" text-h6 Sheet">CSV</h1>
+            </div>
+            <div class="mx-3 d-flex">
+                <div class="px-2">
+                    <v-icon @click="submit" style="background-color: rgb(var(--v-theme-primary),0.3); padding: 20px;"
+                        class="border" color="primary">mdi-check-decagram</v-icon>
                 </div>
-            <v-card-text>
-                <v-table
-                    style="table-layout: fixed;
-                    width: 100%;"
-                    fixed-header
-                    >
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th v-for="value in columns" :style="{ width: value?.width }"  class="text-left">
-                            {{value.title}}
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(item,id) in data"  >
+                <div class="px-2">
+                    <v-icon style="background-color: rgb(var(--v-theme-background)); padding: 20px;" class="border"
+                        color="primary" @click="handleFile()">mdi-database-check-outline</v-icon>
+                </div>
+                <div class="px-2">
+                    <v-icon style="background-color: rgb(var(--v-theme-background)); padding: 20px;" class="border"
+                        color="primary" @click="loadVehicle()">mdi-restart</v-icon>
+                </div>
+            </div>
+        </div>
+        <v-card-text>
+            
+            <v-table style="table-layout: auto; width: max-content;" height="400px" fixed-header>
 
-                            <td>{{ id }}</td>
-                            <td v-for="col in columns">
-                                <div v-if="col.key == 'vehicle_id'">
-                                    <span :class="{'has_error' : errors['data.'+id+'.vehicle_id'] ? true : false }" class="pointer"  @click="OpenModal(id,'vehicle_id',item[col.key])" >{{ item[col.key] }}</span>
-                                </div>
-                                <div v-else-if="col.key == 'body_id'">
-                                    <span :class="{'has_error' : errors['data.'+id+'.body_id'] ? true : false }" class="pointer"  @click="OpenModal(id,'body_id',item[col.key])" >{{ item[col.key] ?? 'None' }}</span>
-                                </div>
-                                <div v-else-if="col.key == 'make_id'">
-                                    <span :class="{'has_error' : errors['data.'+id+'.make_id'] ? true : false }" class="pointer"  @click="OpenModal(id,'make_id',item[col.key])" >{{ item[col.key] ?? 'None' }}</span>
-                                </div>
-                                <div :class="{'has_error' : errors['data.'+id+'.model_id'] ? true : false }" v-else-if="col.key == 'model_id'">
-                                    <span class="pointer"  @click="OpenModal(id,'model_id',item[col.key])" >{{ item[col.key] ?? 'None' }}</span>
-                                </div>
-                                <div :class="{'has_error' : errors['data.'+id+'.variant_id'] ? true : false }" v-else-if="col.key == 'variant_id'">
-                                    <span class="pointer"  @click="OpenModal(id,'variant_id',item[col.key])" >{{ item[col.key] ?? 'None' }}</span>
-                                </div>
-                                <div v-else-if="col.key == 'center_id'">
-                                    <span class="pointer"  @click="OpenModal(id,'center_id',item[col.key])" >{{ item[col.key] ?? 'None' }}</span>
-                                </div>
-                                <div v-else-if="col.disabled">
-                                    <input disabled class=" py-2" :value="item[col.key]" />
-                                </div>
-                                <div v-else>
-                                    <input class="border py-2 px-1" :value="item[col.key]" />
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </v-table>
-            </v-card-text>
-        </v-card>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th v-for="value in columns" :style="{ width: value?.width }" class="text-left">
+                            {{ value.title }}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, id) in data">
 
-        <VehicleTypeModal ref="vehicleTypeModalModal" @update:dailog="hanldeDailog"/>
-        <BodyTypeModal ref="bodyTypeModal" @update:dailog="hanldeDailog"/>
-        <MakeModal ref="makeModal" @update:dailog="hanldeDailog"/>
-        <CenterModal ref="centerModal" @update:dailog="hanldeDailog"/>
+                        <td>{{ id }}</td>
+                        <td v-for="col in columns">
 
-        <ModelModal ref="modelModal" @update:dailog="hanldeDailog"/>
-        <VairantModal ref="variantModal" @update:dailog="hanldeDailog"/>
+                            <div v-if="col.key === 'title'">
+                                <span>{{ item[col.key] }}</span>
+                            </div>
+
+                            <div v-else-if="col.key === 'vehicle_id'">
+                                <span :class="{ has_error: errors['data.' + id + '.vehicle_id'] }" class="pointer"
+                                    @click="OpenModal(id, 'vehicle_id', item[col.key])">
+                                    {{ item[col.key] }}
+                                </span>
+                            </div>
+
+                            <div v-else-if="col.key === 'body_id'">
+                                <span :class="{ has_error: errors['data.' + id + '.body_id'] }" class="pointer"
+                                    @click="OpenModal(id, 'body_id', item[col.key])">
+                                    {{ item[col.key] ?? 'None' }}
+                                </span>
+                            </div>
+
+                            <div v-else-if="col.key === 'make_id'">
+                                <span :class="{ has_error: errors['data.' + id + '.make_id'] }" class="pointer"
+                                    @click="OpenModal(id, 'make_id', item[col.key])">
+                                    {{ item[col.key] ?? 'None' }}
+                                </span>
+                            </div>
+
+                            <div v-else-if="col.key === 'model_id'">
+                                <span :class="{ has_error: errors['data.' + id + '.model_id'] }" class="pointer"
+                                    @click="OpenModal(id, 'model_id', item[col.key])">
+                                    {{ item[col.key] ?? 'None' }}
+                                </span>
+                            </div>
+
+                            <div v-else-if="col.key === 'variant_id'">
+                                <span :class="{ has_error: errors['data.' + id + '.variant_id'] }" class="pointer"
+                                    @click="OpenModal(id, 'variant_id', item[col.key])">
+                                    {{ item[col.key] ?? 'None' }}
+                                </span>
+                            </div>
+
+                            <div v-else-if="col.key === 'center_id'">
+                                <span class="pointer" @click="OpenModal(id, 'center_id', item[col.key])">
+                                    {{ item[col.key] ?? 'None' }}
+                                </span>
+                            </div>
+
+                            <div v-else-if="col.disabled">
+                                <input disabled class="py-2" :value="item[col.key]" />
+                            </div>
+
+                            <div v-else>
+                                <input class="border py-2 px-1" :value="item[col.key]" />
+                            </div>
+
+                        </td>
+
+                    </tr>
+                </tbody>
+            </v-table>
+        </v-card-text>
+    </v-card>
+
+    <VehicleTypeModal ref="vehicleTypeModalModal" @update:dailog="hanldeDailog" />
+    <BodyTypeModal ref="bodyTypeModal" @update:dailog="hanldeDailog" />
+    <MakeModal ref="makeModal" @update:dailog="hanldeDailog" />
+    <CenterModal ref="centerModal" @update:dailog="hanldeDailog" />
+
+    <ModelModal ref="modelModal" @update:dailog="hanldeDailog" />
+    <VairantModal ref="variantModal" @update:dailog="hanldeDailog" />
 
 </template>
 <script>
@@ -102,21 +134,21 @@ export default {
     data() {
 
         return {
-            scrap:[],
-            auction_id:this.$route.params.id,
-            selectedRow:null,
+            scrap: [],
+            auction_id: this.$route.params.id,
+            selectedRow: null,
             loading: false,
             data: [],
-            errors:{},
+            errors: {},
             columns: columns,
-            csv:null,
+            csv: null,
         }
     },
     mounted() {
 
-      
+
         this.loadVehicle()
-        
+
     },
     methods: {
 
@@ -151,7 +183,7 @@ export default {
             try {
 
                 const id = this.$route.params.id;
-                let res = await Auction.csvUpdate(id, {data: this.data});   
+                let res = await Auction.csvUpdate(id, { data: this.data });
                 this.$alertStore.add(res.message, 'success');
                 this.loadVehicle();
 
@@ -166,11 +198,11 @@ export default {
                         this.errors[key] = element;
                     }
                 }
-                
+
             } finally {
                 this.loading = false;
-            }  
-                
+            }
+
         },
         async handleFile() {
 
@@ -194,7 +226,7 @@ export default {
                 this.loading = false;
             }
         },
-        OpenModal(row,key,value) {
+        OpenModal(row, key, value) {
             switch (key) {
                 case 'vehicle_id':
                     this.$refs.vehicleTypeModalModal.open(row, value);
@@ -206,22 +238,22 @@ export default {
                     this.$refs.makeModal.open(row, value);
                     break;
                 case 'model_id':
-                this.$refs.modelModal.open(row, value);
+                    this.$refs.modelModal.open(row, value);
                     break;
                 case 'variant_id':
                     debugger
-                this.$refs.variantModal.open(row, value);
+                    this.$refs.variantModal.open(row, value);
                     break;
                 case 'center_id':
                     this.$refs.centerModal.open(row, value);
                     break;
             }
         },
-        updateCell(rowIndex, key, value){
+        updateCell(rowIndex, key, value) {
             this.data[rowIndex][key] = value;
         },
-        hanldeDailog(row,key,e) {
-            this.data[row][key] = e; 
+        hanldeDailog(row, key, e) {
+            this.data[row][key] = e;
         }
 
 
@@ -232,29 +264,29 @@ export default {
 
 </script>
 
-<style lang="css" scoped >
-
+<style lang="css" scoped>
 .truncate {
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
 
-td{
-    width: auto;
-}
-
-.activeRow{
-    background-color: red!important;
+td {
+    white-space: nowrap;
 }
 
 
-.pointer{
+.activeRow {
+    background-color: red !important;
+}
+
+
+.pointer {
     cursor: pointer;
 }
 
-.has_error{
-        background: rgb(var(--v-theme-danger),0.2);
+.has_error {
+    background: rgb(var(--v-theme-danger), 0.2);
     padding: 14px 0px;
 }
 </style>

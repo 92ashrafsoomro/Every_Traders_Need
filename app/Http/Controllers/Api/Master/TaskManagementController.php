43307f68c419d8e06afde445f1533721a908519f
daprimeg <owais.dapp@gmail.com>
2026-01-30 20:45:19 +0500

    upu

--- app/Http/Controllers/Api/Master/TaskManagementController.php
+++ app/Http/Controllers/Api/Master/TaskManagementController.php
@@ -46,6 +46,10 @@ class TaskManagementController extends Controller
         $page   = $request->input('page', 1);
         $offset = ($page - 1) * $length;
 
+       
+
+
+
         //Query
         $query = TaskManagement::with(['auctionHouse','auctionType']);
 
@@ -133,7 +137,15 @@ class TaskManagementController extends Controller
             COUNT(CASE WHEN status = 'publish' THEN 1 END) as total_publish,
             SUM(lots) as total_lots,
             SUM(lots) as published_lots
-        ")->first();
+
+        ")
+        ->whereDate('date',Carbon::now())
+        ->first();
+        
+        $all = TaskManagement::count('*');
+        $upcomming = TaskManagement::whereDate('date', '>', now())->count('id');
+        $update =  TaskManagement::whereDate('date',now()->addDays(1))->count('id');
+        $final = TaskManagement::whereDate('date',now())->count('id');
 
      
       
@@ -155,6 +167,12 @@ class TaskManagementController extends Controller
         
         return response()->json([
             'data' => $stats,
+            'counters' => [
+                'all' => $all,
+                'upcomming' => $upcomming,
+                'update' => $update,
+                'final' => $final,
+            ]
         ],200);
         
     }

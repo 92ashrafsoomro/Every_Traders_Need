a24f468956101cf235b1606b58b3ff7358bb09ac
mshakeebraza <shakeebraza9@gmail.com>
2026-02-02 20:07:56 +0500

    chnages in auction page in admin

--- resources/js/views/admin/auction/index.vue
+++ resources/js/views/admin/auction/index.vue
@@ -1,12 +1,13 @@
 <template>
-    <user-title-bar title="Auction List">
-
+    <user-title-bar style="padding-top: 0 !important; margin-top: 0 !important;">
         <div>
-            <v-card-title class="d-flex cursor-pointer " @click="showFilters = !showFilters">
+            <v-card-title class="d-flex cursor-pointer w-100" @click="showFilters = !showFilters">
+            <div class="d-flex justify-space-between w-100">
+            <h1 class="text-lg-h5 text-md-h3 text-h4 mb-2 font-weight-bold">Auction List</h1>
                 <span class="text-h6">
                     Filters
                 </span>
-
+            </div>
                 <v-icon color="primary">
                     {{ showFilters ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
                 </v-icon>
@@ -15,44 +16,19 @@
             <v-expand-transition>
                 <div v-show="showFilters">
                     <v-container fluid>
-                        <div class="d-flex flex-wrap ga-2 ">
+                        <div class="d-flex justify flex-wrap ga-2 ">
                             <v-text-field density="comfortable" variant="outlined" clearable v-model="filter.table_id"
                                 label="Id" min-width="130px" max-width="130px" />
 
-                            <PlateformDropdown min-width="250px" max-width="274px" v-model="filter.platform"
+                            <PlateformDropdown min-width="350px" max-width="374px" v-model="filter.platform"
                                 label="Auction House" clearable variant="outlined" base-color="white"
                                 density="comfortable" />
-
-                            <!-- <v-text-field density="comfortable" variant="outlined" clearable v-model="filter.name"
-                                    label="Auction Name " /> -->
-                            <AuctionTypeDropdown min-width="190px" max-width="274px" v-model="filter.auction_type"
+                                
+                            <AuctionTypeDropdown min-width="350px" max-width="374px" v-model="filter.auction_type"
                                 label="Auction Type" variant="outlined" base-color="white" clearable
                                 density="comfortable" />
 
-                            <!-- item-title="title"
-                                    item-value="id" -->
-
-                            <!-- <StatusDropdown label="Status "
-                                    variant="outlined"   density="comfortable"
-                                    min-width="190px" max-width="274px" /> -->
-
-                            <!-- <StatusDropdown
-                                        v-model="filter.status"
-                                        label="Status "
-                                        item-title="title"
-                                        item-value="id"
-                                        clearable
-                                        variant="outlined"
-                                        density="comfortable"
-                                        min-width="190px" max-width="274px" 
-                                    /> -->
-                            <!-- item-title="name"
-                                    item-value="id" -->
-
-                            <!-- <v-select min-width="190px" max-width="274px" density="comfortable" variant="outlined" :items="['Draft', 'Planed']"
-                                     clearable v-model="filter.status" label="Status " /> -->
-
-                            <v-text-field density="comfortable" variant="outlined" min-width="340px" max-width="274px"
+                            <v-text-field density="comfortable" variant="outlined" min-width="350px" max-width="374px"
                                 type="date" v-model="filter.auction_date" label="Auction Date" />
                             <div class="">
                                 <v-btn style="height: 47px; ;" variant="outlined" @click="loadItems(true)" class="mr-2">
@@ -63,149 +39,11 @@
                         </div>
                         <!-- <AuctionWeek/> -->
                     </v-container>
-                    <div style="max-width: 1400px;" class="mx-auto pa-2">
-                        <v-row no-gutters="" class="ga-2">
-                            <!-- Total Auctions -->
-
-
-                            <!-- Total Auctions -->
-                            <v-col cols="12" sm="6" md="" lg="" class="">
-
-                                <v-card class="border-bottom border border-sm">
-                                    <v-container>
-
-                                        <div class="d-flex h-6">
-                                            <div style="background: #0080ff38; border-radius: 22px; width: 40px; height: 40px;"
-                                                class="circle d-flex justify-center align-center">
-                                                <v-icon size="22" color="primary">mdi-store-clock</v-icon>
-                                            </div>
-
-                                            <span class="text-h4 px-3">{{ auctionCounter.total_auctions }}</span>
-                                        </div>
-
-
-                                        <div class="pt-3 text-body-1 text-whiteLite ">Total Auctions</div>
-                                        <div class="pt-2 d-flex">
-                                            <div class="text-body-2 pr-2">
-                                                <span class="white">Live Auctions: </span>
-                                                <span class=""
-                                                    style="background-color: rgba(var(--v-theme-background)); padding: 8px; border-radius: 4px; ">
-                                                    {{ auctionCounter.live_auctions }}</span>
-                                            </div>
-                                            <div class=" text-body-2 pl-2">
-                                                <span class="white">Time Auctions: </span>
-                                                <span class=""
-                                                    style="background-color: rgba(var(--v-theme-background)); padding: 8px; border-radius: 4px; ">
-                                                    {{
-                                                        auctionCounter.time_auctions }}</span>
-                                            </div>
-                                        </div>
-
-
-                                    </v-container>
-                                </v-card>
-                            </v-col>
-                            <!-- Total Auctions -->
-                            <v-col cols="12" sm="6" md="" lg="" class="">
-
-                                <v-card class="border-bottom-inprogress w-100 border border-sm">
-                                    <v-chip text-color="white" size="small"
-                                        class="bg-danger position-absolute mt-5 right-0  "
-                                        style="border-radius: 3px 0 0 3px;" label>
-                                        <v-icon size="10" color="white" class="mr-2">
-                                            mdi-circle
-                                        </v-icon> Live
-                                    </v-chip>
-                                    <v-container>
-                                        <div class="d-flex h-6">
-                                            <div class=" bg-danger-lite d-flex justify-center align-center live-circle"
-                                                style="border-radius: 22px; width: 40px; height: 40px;">
-                                                <v-icon size="32" color="danger" class="">
-                                                    mdi-radiobox-marked
-                                                </v-icon>
-                                            </div>
-
-                                            <span class="text-h4 px-3">{{ auctionCounter.in_progressAuctions }}</span>
-                                        </div>
-
-                                        <div class="pt-3 text-body-1 text-whiteLite ">Inprogress Auctions</div>
-                                        <div class="pt-2 d-flex justify-space-between">
-                                            <div class="text-body-2 pr-2">
-                                                <span class="white">Vehicles:</span>
-                                                <span class=""
-                                                    style="background-color: rgba(var(--v-theme-background)); padding: 8px; border-radius: 4px; ">
-                                                    {{ auctionCounter.vehicles_in_progress_auctions }}</span>
-                                            </div>
-                                            <div class="text-body-2">View</div>
-
-                                        </div>
-
-
-                                    </v-container>
-                                </v-card>
-                            </v-col>
-                            <!-- Total Auctions -->
-                            <v-col cols="12" sm="6" md="" lg="" class="">
-
-                                <v-card class="border-bottom  border border-sm">
-                                    <v-container>
-
-                                        <div class="d-flex h-6">
-                                            <div style="background: #0080ff38; border-radius: 22px; width: 40px; height: 40px;"
-                                                class="circle d-flex justify-center align-center">
-                                                <v-icon size="32" color="primary">mdi-bash</v-icon>
-                                            </div>
-                                            <span class="text-h4 px-3">{{ auctionCounter.total_vehicles }}</span>
-                                        </div>
-
-                                        <div class="pt-3 text-body-1 text-whiteLite ">Total Vehicles</div>
-                                        <div class="pt-2 d-flex">
-                                            <div class="text-body-2 pr-2">
-                                                <span class="white">Sold:</span>
-                                                <span class=""
-                                                    style="background-color: rgba(var(--v-theme-background)); padding: 8px; border-radius: 4px; ">
-                                                    {{ auctionCounter.sold_vehicles }}</span>
-                                            </div>
-                                        </div>
-
-
-                                    </v-container>
-                                </v-card>
-                            </v-col>
-
-                            <v-col cols="12" sm="6" md="" lg="" class="">
-
-                                <v-card class="border-bottom  border border-sm">
-                                    <v-container>
-                                        <div class="d-flex h-6">
-                                            <div style="background: #0080ff38; border-radius: 22px; width: 40px; height: 40px;"
-                                                class="circle d-flex justify-center align-center">
-                                                <v-icon size="32" color="primary">mdi-repeat-variant</v-icon>
-                                            </div>
-                                            <span class="text-h4 px-3">{{ auctionCounter.vehicles_in_reauction }}</span>
-                                        </div>
-
-                                        <div class="pt-3 text-body-1 text-whiteLite ">Vehicle in reauctions</div>
-                                        <div class="pt-2 d-flex">
-                                            <div class="text-body-2 pr-2">
-                                                <span class="white">Remaining:</span>
-                                                <span class=""
-                                                    style="background-color: rgba(var(--v-theme-background)); padding: 8px; border-radius: 4px; ">
-                                                    {{ auctionCounter.remaining }}</span>
-                                            </div>
-
-                                        </div>
-
-
-                                    </v-container>
-                                </v-card>
-                            </v-col>
-                        </v-row>
-                    </div>
+     
 
                     <div class="content-scroll d-flex mt-6 w-100 ga-3">
                         <div v-for="item in status" :key="item.value"
-                            class="border rounded bg-surface-variant-1 pa-3 cursor-pointer" style="width:13.5%"
+                            class="border rounded bg-surface-variant-1 pa-3 cursor-pointer" style="width:15.5%"
                             @click="filter.status = item.value"
                             :class="{ 'bg-primary text-white': filter.status === item.value }">
                             <div class="d-flex justify-space-between ">
@@ -216,7 +54,7 @@
                                         class="text-primary auction-svg" xmlns="http://www.w3.org/2000/svg">
                                         <path fill="currentColor"
                                             d="M504.971 199.362l-22.627-22.627c-9.373-9.373-24.569-9.373-33.941 0l-5.657 5.657L329.608 69.255l5.657-5.657c9.373-9.373 9.373-24.569 0-33.941L312.638 7.029c-9.373-9.373-24.569-9.373-33.941 0L154.246 131.48c-9.373 9.373-9.373 24.569 0 33.941l22.627 22.627c9.373 9.373 24.569 9.373 33.941 0l5.657-5.657 39.598 39.598-81.04 81.04-5.657-5.657c-12.497-12.497-32.758-12.497-45.255 0L9.373 412.118c-12.497 12.497-12.497 32.758 0 45.255l45.255 45.255c12.497 12.497 32.758 12.497 45.255 0l114.745-114.745c12.497-12.497 12.497-32.758 0-45.255l-5.657-5.657 81.04-81.04 39.598 39.598-5.657 5.657c-9.373 9.373-9.373 24.569 0 33.941l22.627 22.627c9.373 9.373 24.569 9.373 33.941 0l124.451-124.451c9.372-9.372 9.372-24.568 0-33.941z" />
-                                    </svg> <span class="ml-1">0</span> </div>
+                                    </svg> <span class="ml-1"> {{ statusCounts[item.value] }}</span> </div>
                             </div>
                         </div>
 
@@ -233,39 +71,39 @@
     </user-title-bar>
 
     <v-container max-width="1400px">
-        <v-row no-gutters class="mt-3">
+        <v-row no-gutters class="">
             <v-col cols="12">
 
-                <div class="d-flex justify-space-between d-md-flex py-4">
+                <!-- <div class="d-flex justify-space-between d-md-flex py-4">
                     <div class="d-flex align-center pb-2 pb-lg-0 pb-md-0">
                         <v-select v-model="filter.length" :items="[10, 25, 50, 100]" density="compact"
                             variant="outlined" max-width="150px" class="mr-2" />
                         <div class="align-self-center pl-2">{{ filter.offset }} - {{ Math.min(filter.length, total) }}
                             of {{ total }} Records </div>
-                    </div>
+                    </div> 
 
-                    <!-- <div class="d-flex w-lg-75 justify-end pb-2 pb-lg-0 pb-md-0  "> -->
-                    <!-- <v-text-field v-model="filter.search" label="Search..." variant="outlined" density="compact"
+                   <div class="d-flex w-lg-75 justify-end pb-2 pb-lg-0 pb-md-0  "> 
+                    <v-text-field v-model="filter.search" label="Search..." variant="outlined" density="compact"
                             max-width="400px" clearable />
-                        <div class="pl-2"> -->
-                    <!-- <v-btn base-color="#bdbdbd" style="height: 44px;" variant="outlined" @click="loadItems">
+                        <div class="pl-2"> 
+                     <v-btn base-color="#bdbdbd" style="height: 44px;" variant="outlined" @click="loadItems">
                                 <v-icon icon="mdi-magnify"></v-icon>
-                            </v-btn> -->
-                    <!-- </div> -->
-                    <!-- </div> -->
-                    <!-- <div class="pl-lg-2 pt-lg-0 pt-md-0">
+                            </v-btn> 
+                        </div>
+                     </div>
+                   <div class="pl-lg-2 pt-lg-0 pt-md-0">
                         <v-btn to="/admin/auction/create" color="primary" style="height: 44px;" variant="flat"
                             @click="loadItems">
                             <v-icon icon="mdi-plus"></v-icon>
                         </v-btn>
-                    </div> -->
-                </div>
+                    </div>
+                </div> -->
             </v-col>
             <v-col cols="12" class="mt-2">
                 <div class="border">
-                    <v-data-table-server :loading="loading" :headers="headers" :items="items" :items-length="total"
+                    <v-data-table-server  :loading="loading" :headers="headers" :items="items" :items-length="total"
                         hover item-value="id" @update:options="loadItems" :lastPage="last_page"
-                        style="height: 500px !important;">
+                        style="height: 600px !important;">
 
                         <template #item.name="{ item }" style="width: 100px; ba">
                             <span>
@@ -378,9 +216,8 @@ export default {
             loading: true,
             headers: [
                 { title: "ID", value: "id", sortable: false },
-                { title: "Auction Id", value: "table_id", sortable: false },
+                { title: "Auc Id", value: "table_id", sortable: false },
                 { title: "Name", value: "name" },
-                { title: "Type", value: "auction_type.title" },
                 { title: "Au House", value: "platform.name" },
                 { title: "Status", value: "auction_status.title" },
                 { title: "Start Date", value: "auction_date" },
@@ -396,6 +233,31 @@ export default {
         this.$themeStore.menuType = 'collapsed';
     },
     computed: {
+
+    statusCounts() {
+        const counts = {
+        '1': 0, 
+        '2': 0, 
+        '3': 0,
+        '4': 0, 
+        '5': 0, 
+        '6': 0, 
+        }
+
+    
+        if (!Array.isArray(this.items)) {
+        return counts
+        }
+
+        this.items.forEach(item => {
+        const statusId = String(item?.auction_status?.id)
+        if (counts[statusId] !== undefined) {
+            counts[statusId]++
+        }
+        })
+
+        return counts 
+    }
 
     },
     watch: {
@@ -442,8 +304,18 @@ export default {
             }
         },
         formatDate(date) {
-            if (!date) return ''
-            return date.split('T')[0].split(' ')[0]
+        if (!date) return '-'
+
+        const d = new Date(date)
+
+        return d.toLocaleString("en-GB", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false
+        })
         },
         async loadItems() {
 
@@ -509,4 +381,8 @@ export default {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+:deep(th) {
+  white-space: nowrap !important;
+}
+</style>
